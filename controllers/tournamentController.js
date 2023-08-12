1206caf4f8fc260ee1df9e6e9c85e166eeea4bfc
dhanno75 <pdhananjay774@gmail.com>
2023-08-12 18:54:52 +0530

    minor changes

--- controllers/tournamentController.js
+++ controllers/tournamentController.js
@@ -26,7 +26,8 @@ export const createTournament = async (req, res) => {
         endDate,
         status: "upcoming",
         participantsId,
-        banner,
+        banner:
+          "https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
       });
 
       return res.status(201).json({
